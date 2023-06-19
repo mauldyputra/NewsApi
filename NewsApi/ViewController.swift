7d71ea58e5e5b34f867f5790740891e350c483cc
Mauldy Putra <mauldy.putra@aryanoble.co.id>
2023-06-19 13:28:33 +0700

    Initial Commit

---
+++ NewsApi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApi
+//
+//  Created by mauldy.putra on 19/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
