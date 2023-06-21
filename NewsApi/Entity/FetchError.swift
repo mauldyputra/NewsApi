ea623edecc86c8e28d657279ad862bff01db37ac
Mauldy Putra <mauldy.putra@aryanoble.co.id>
2023-06-21 21:29:31 +0700

    refactor

--- NewsApi/Entity/FetchError.swift
+++ NewsApi/Entity/FetchError.swift
@@ -10,5 +10,3 @@ import Foundation
 enum FetchError: Error {
     case failed
 }
-
-
--- NewsApi/Entity/NewsSource.swift
+++
@@ -1,8 +0,0 @@
-//
-//  NewsSource.swift
-//  NewsApi
-//
-//  Created by mauldy.putra on 19/06/23.
-//
-
-import Foundation
